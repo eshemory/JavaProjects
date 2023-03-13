b87163deef0ebfa5036428c80db5a2ced9bd4683
eshemory <107089429+eshemory@users.noreply.github.com>
2023-03-13 15:12:49 -0400

    Update HotDogStand.java

--- HotDogStand.java
+++ HotDogStand.java
@@ -8,34 +8,58 @@ package exam4;
  *
  */
 public class HotDogStand {
-	private int id;		// Hot dog stand's id number
+	private int id;				// Hot dog stand's id number
 	private int hotDogsSold;	// Number of hot dogs sold
 	private static int totalHotDogsSold;
-	
-	public HotDogStand(int id, int hotDogsSold)
-	{
+
+	/**
+	 * Creates a new hot dog stand with the specified id and number of hot dogs sold.
+	 * 
+	 * @param id the id number of the hot dog stand
+	 * @param hotDogsSold the number of hot dogs sold by the hot dog stand
+	 */
+	public HotDogStand(int id, int hotDogsSold) {
 		this.id = id;
 		this.hotDogsSold = hotDogsSold;
-		this.totalHotDogsSold += hotDogsSold;
+		totalHotDogsSold += hotDogsSold;  // add the hot dogs sold by this stand to the total
 	}
-	
-	public void justSold()
-	{
-		totalHotDogsSold = hotDogsSold++;
+
+	/**
+	 * Records the sale of a hot dog by this hot dog stand.
+	 * 
+	 * Updates both the number of hot dogs sold by this stand and the total number of hot dogs sold.
+	 */
+	public void justSold() {
+		hotDogsSold++;
+		totalHotDogsSold++;
 	}
-	
-	public int getSold()
-	{
+
+	/**
+	 * Returns the number of hot dogs sold by this hot dog stand.
+	 * 
+	 * @return the number of hot dogs sold by this hot dog stand
+	 */
+	public int getSold() {
 		return hotDogsSold;
 	}
-	
-	public int getIdentity()
-	{
+
+	/**
+	 * Returns the id number of this hot dog stand.
+	 * 
+	 * @return the id number of this hot dog stand
+	 */
+	public int getIdentity() {
 		return id;
 	}
-	
-	public static int getTotalSold()
-	{
+
+	/**
+	 * Returns the total number of hot dogs sold by all hot dog stands.
+	 * 
+	 * @return the total number of hot dogs sold by all hot dog stands
+	 */
+	public static int getTotalSold() {
 		return totalHotDogsSold;
 	}
 }
+
+
